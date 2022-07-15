bf291f2514ef6589db7cbf8c9e3c5af5ea72e43b
Martasantenbo <martasan.donuts@gmail.com>
2022-07-15 16:58:27 +0200

    add(main.py) .

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import string
 password = ""
 length = int(input("Input length of your password "))
 for i in range(length):
-  position = random.randint(0,len(string.printable)-1)
-  password = password + string.printable[position]
+  position = random.randint(0,len(string.ascii_letters)-1)
+  password = password + string.ascii_letters[position]
 print(password)
