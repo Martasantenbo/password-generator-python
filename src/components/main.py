97acb0e0dfbfba6cd438abd74c4a8bea90a5d124
Martasantenbo <martasan.donuts@gmail.com>
2022-07-15 19:34:39 +0200

    add(index.html, main.css, main.py) added html/css

--- src/components/main.py
+++ src/components/main.py
@@ -2,7 +2,7 @@ import random
 import string
 
 password = ""
-length = int(input("Input length of your password "))
+length = int(input())
 for i in range(length):
   position = random.randint(0,len(string.ascii_letters)-1)
   password = password + string.ascii_letters[position]
